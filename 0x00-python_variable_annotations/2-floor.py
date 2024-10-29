402731ed4c3f9591f592ef726b48fa70c9adea47
yassineboucham <yassineboucham2002@gmail.com>
2024-10-29 12:15:12 +0100

    2

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""
+the floor utility
+"""
+
+
+def floor(n: float) -> int:
+    """Get the floor value of a float"""
+    if n >= 0:
+        return int(n)
+    else:
+        return int(n) - 1
