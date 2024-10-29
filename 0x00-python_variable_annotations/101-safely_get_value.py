5dad60d33cde0ca68bb6cae63f0111531a35ac57
yassineboucham <yassineboucham2002@gmail.com>
2024-10-29 12:55:01 +0100

    all

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+"""TypeVar"""
+
+
+from typing import Mapping, Any, TypeVar, Union
+
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping,
+                     key: Any,
+                     default: Union[T, None] = None
+                     ) -> Union[Any, T]:
+    """TypeVar wierd stuff"""
+    if key in dct:
+        return dct[key]
+    else:
+        return defaul
--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+"""
+Fixing the annotation typing
+"""
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Doing stuff"""
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
