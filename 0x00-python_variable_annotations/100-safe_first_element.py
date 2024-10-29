2978e615722d57d898e30bb2e09db87e2a8e58b7
yassineboucham <yassineboucham2002@gmail.com>
2024-10-29 12:53:06 +0100

    100

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+"""
+list or a dict as an argument only need their argument to be somehow
+“list-like” or “dict-like”. A specific meaning of “list-like” or “dict-like”
+(or something-else-like) is called a “duck type”
+"""
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """duck type"""
+    if lst:
+        return lst[0]
+    else:
+        return None
