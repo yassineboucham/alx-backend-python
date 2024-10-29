870ae516074c40cd9a9715337565da8b79ba4241
yassineboucham <yassineboucham2002@gmail.com>
2024-10-29 12:51:27 +0100

    9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+"""
+From dynamically typed to strongly typed
+"""
+from typing import List, Tuple, Union, Sequence, Iterable
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Converting statically typed to dynamically typed"""
+    return [(i, len(i)) for i in lst]
