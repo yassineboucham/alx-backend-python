279757c25dff1188a164cb31fb3ab81dccec0f3f
yassineboucham <yassineboucham2002@gmail.com>
2024-10-29 12:24:54 +0100

    6

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""
+Sum of floats
+"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """sum of floats and ints"""
+    s: float = 0
+    for n in mxd_lst:
+        s += n
+    return s
